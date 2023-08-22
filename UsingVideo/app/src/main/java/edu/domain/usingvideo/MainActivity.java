154ede2b4f16d274dd517a2ab2a35434c491004c
Artem Tribunskiy <fazaprofit@yandex.ru>
2023-08-22 16:58:00 +0600

    2023-08-22 second commit Player work2home

--- UsingVideo/app/src/main/java/edu/domain/usingvideo/MainActivity.java
+++ UsingVideo/app/src/main/java/edu/domain/usingvideo/MainActivity.java
@@ -2,6 +2,7 @@ package edu.domain.usingvideo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -11,6 +12,9 @@ import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.SeekBar;
 import android.widget.VideoView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,11 +38,21 @@ AudioManager audioManager;
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 
         volumeSeekBar = findViewById(R.id.volumeSeekBar);
-        volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setMax(mediaPlayer.getDuration());
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                volumeSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000);
+
         volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+                //audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+                if(b){
+                    mediaPlayer.seekTo(i);
+                }
             }
 
             @Override
@@ -54,16 +68,25 @@ AudioManager audioManager;
         // mediaPlayer.pause();
     }
 
-    public void Play(View view) {
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void playPause(View view) {
 
         if (!playOn) {
             //oneButton.setText("PAUSE");
             mediaPlayer.start();
+            oneButton.setImageDrawable(getResources().getDrawable(R.drawable.baseline_pause_24, getApplicationContext().getTheme()));
+
         } else {
             //oneButton.setText("PLAY");
             mediaPlayer.pause();
+            oneButton.setImageDrawable(getResources().getDrawable(R.drawable.baseline_play_arrow_24));
         }
         playOn = !playOn;
     }
 
+    public void nextTrack(View view) {
+    }
+
+    public void prevTrack(View view) {
+    }
 }
